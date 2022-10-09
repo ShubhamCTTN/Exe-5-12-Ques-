f16d3d6876f46f9b0f1e90358e196ef7b6758121
ShubhamCTTN <114070622+ShubhamCTTN@users.noreply.github.com>
2022-10-09 11:33:30 +0530

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -3,26 +3,30 @@
 package exe5;
 
 interface Java {
-	default void get(int a, int b) {
-		mul(a,b);
-		add(a,b);
-	}
-
+	//Declaring Private method
 	private void mul(int a, int b) {
 		System.out.println("Answer is provided by the private method : ");
-		System.out.println(a*b);
+		System.out.println(a * b);
 	}
-	
+
+	//Declaring Private method
 	private void add(int a, int b) {
 		System.out.println("Answer is provided by the private method : ");
-		System.out.println(a+b);
+		System.out.println(a + b);
 	}
+
+	//Calling those private method in default method
+	default void get(int a, int b) {
+		mul(a, b);
+		add(a, b);
+	}
+
 }
 
 public class Question1 implements Java {
 	public static void main(String[] args) {
 		Java obj1 = new Question1();
-		obj1.get(4,2);
+		obj1.get(4, 2);
 	}
 
 }
