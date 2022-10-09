b490a3c655d0e6de621bc1d789d83495b7cc1258
ShubhamCTTN <114070622+ShubhamCTTN@users.noreply.github.com>
2022-10-09 11:40:33 +0530

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -6,6 +6,8 @@ import java.util.stream.IntStream;
 
 public class Question4 {
 	public static void main(String[] args) {
+		// generating number stream using iterate method
 		IntStream.iterate(0, i -> i < 10, i -> i + 1).forEach(System.out::println);
 	}
 }
+
