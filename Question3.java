f80b8a267a7f6f347aaa204a1bd16c4eb962e240
ShubhamCTTN <114070622+ShubhamCTTN@users.noreply.github.com>
2022-10-09 11:38:46 +0530

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -9,6 +9,7 @@ public class Question3 {
 	public static void main(String[] args) {
 
 		System.out.println("Stream.rangeClosed()");
+		// Printing the numbers using rangeClosed method
 		IntStream.rangeClosed(1, 10).forEach(i -> System.out.print(i + " "));
 	}
 
