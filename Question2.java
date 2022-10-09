a18f6b2060c8b4893cbc8e5c6fa9231e9b7ff742
ShubhamCTTN <114070622+ShubhamCTTN@users.noreply.github.com>
2022-10-09 11:36:12 +0530

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -7,12 +7,15 @@ import java.util.*;
 public class Question2 {
 
 	public static void main(String[] args) {
+		// Converting array as a list
 		List<Integer> OrderedList = Arrays.asList(1, 3, 5, 7, 9, 13, 16, 17, 19);
 
 		System.out.println("takeWhile");
+		// Performing takeWhile operation
 		OrderedList.stream().takeWhile(e -> e % 2 != 0).forEach(e -> System.out.println(e));
 
 		System.out.println("dropWhile");
+		// Performing dropWhile operation
 		OrderedList.stream().dropWhile(e -> e % 2 != 0).forEach(e -> System.out.println(e));
 
 	}
